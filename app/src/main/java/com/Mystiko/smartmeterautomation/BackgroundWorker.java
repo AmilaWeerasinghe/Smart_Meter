2b9ffbf40de0d7798cbf9437a3fb4928f16ed9af
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2019-03-24 11:19:21 +0530

    new interface and connection

--- app/src/main/java/com/Mystiko/smartmeterautomation/BackgroundWorker.java
+++ app/src/main/java/com/Mystiko/smartmeterautomation/BackgroundWorker.java
@@ -16,61 +16,57 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 
+
 public class BackgroundWorker extends AsyncTask<String,Void,String> {
     Context context;
     AlertDialog alertDialog;
-
-    BackgroundWorker(Context ctx){
-        context=ctx;
-
+    BackgroundWorker (Context ctx) {
+        context = ctx;
     }
-
     @Override
-    protected String doInBackground(String... voids) {
-        String type =voids[0];
-        String login_url="https://final-business.000webhostapp.com/login.php";
-        if(type.equals("login")){
+    protected String doInBackground(String... params) {
+        String type = params[0];
+        String login_url = "http://192.168.8.100/login.php";
+        if(type.equals("login")) {
             try {
-                String user_name =voids[1];
-                String password =voids[2];
-                URL url=new URL(login_url);
-                HttpURLConnection httpURLConnection=(HttpURLConnection)url.openConnection();
+                String user_name = params[1];
+                String password = params[2];
+                URL url = new URL(login_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
                 httpURLConnection.setRequestMethod("POST");
                 httpURLConnection.setDoOutput(true);
                 httpURLConnection.setDoInput(true);
-                OutputStream outputStream=httpURLConnection.getOutputStream();
-                BufferedWriter bufferedWriter=new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
-                String post_data= URLEncoder.encode("user_name","UTF-8")+"="+URLEncoder.encode(user_name,"UTF-8")+"&"
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String post_data = URLEncoder.encode("user_name","UTF-8")+"="+URLEncoder.encode(user_name,"UTF-8")+"&"
                         +URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8");
                 bufferedWriter.write(post_data);
                 bufferedWriter.flush();
                 bufferedWriter.close();
                 outputStream.close();
-                InputStream inputStream=httpURLConnection.getInputStream();
-                BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
                 String result="";
                 String line="";
-                while((line=bufferedReader.readLine())!= null){
-                    result = result+line;
+                while((line = bufferedReader.readLine())!= null) {
+                    result += line;
                 }
                 bufferedReader.close();
                 inputStream.close();
                 httpURLConnection.disconnect();
                 return result;
-
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
         }
         return null;
     }
 
     @Override
     protected void onPreExecute() {
-        alertDialog =new AlertDialog.Builder(context).create();
+        alertDialog = new AlertDialog.Builder(context).create();
         alertDialog.setTitle("Login Status");
     }
 
--- app/src/main/java/com/Mystiko/smartmeterautomation/MainActivity.java
+++ app/src/main/java/com/Mystiko/smartmeterautomation/MainActivity.java
@@ -1,33 +1,32 @@
 package com.Mystiko.smartmeterautomation;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity  {
-    Button LogInB;
-    EditText UsernameEt,PasswordEt;
 
+
+
+public class MainActivity extends AppCompatActivity {
+    EditText UsernameEt, PasswordEt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       LogInB=(Button)findViewById(R.id.BLogIn);
-       UsernameEt=(EditText)findViewById(R.id.EtUserame);
-       PasswordEt=(EditText)findViewById(R.id.EtPassword);
+        UsernameEt = (EditText)findViewById(R.id.etUserName);
+        PasswordEt = (EditText)findViewById(R.id.etPassword);
     }
 
-    public void OnLogin(View view){
-        String username=UsernameEt.getText().toString();
-        String password=PasswordEt.getText().toString();
-        String type="login ";
-        BackgroundWorker backgroundWorker=new BackgroundWorker(this);
-        backgroundWorker.execute(type,username,password);
-
+    public void OnLogin(View view) {
+        String username = UsernameEt.getText().toString();
+        String password = PasswordEt.getText().toString();
+        String type = "login";
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type, username, password);
     }
-
 
 }
