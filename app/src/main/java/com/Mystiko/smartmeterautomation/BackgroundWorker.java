a5277c2617b90c1858316c9031a4b193670ffeb3
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2019-03-24 14:08:57 +0530

    Succesfully Created Login and Register Interfaces PHP Scripts for localhost WAMP server and MYSQL databse

--- app/src/main/java/com/Mystiko/smartmeterautomation/BackgroundWorker.java
+++ app/src/main/java/com/Mystiko/smartmeterautomation/BackgroundWorker.java
@@ -27,6 +27,8 @@ public class BackgroundWorker extends AsyncTask<String,Void,String> {
     protected String doInBackground(String... params) {
         String type = params[0];
         String login_url = "http://192.168.8.100/login.php";
+        String register_url = "http://192.168.8.100/register.php";
+
         if(type.equals("login")) {
             try {
                 String user_name = params[1];
@@ -60,6 +62,47 @@ public class BackgroundWorker extends AsyncTask<String,Void,String> {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+        else if(type.equals("register")){
+            try {
+                String name = params[1];
+                String surname = params[2];
+                String age = params[3];
+                String user_name = params[4];
+                String password = params[5];
+                URL url = new URL(register_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String post_data = URLEncoder.encode("name","UTF-8")+"="+URLEncoder.encode(name,"UTF-8")+"&"
+                        +URLEncoder.encode("surname","UTF-8")+"="+URLEncoder.encode(surname,"UTF-8")+"&"
+                        +URLEncoder.encode("age","UTF-8")+"="+URLEncoder.encode(age,"UTF-8")+"&"
+                        +URLEncoder.encode("user_name","UTF-8")+"="+URLEncoder.encode(user_name,"UTF-8")+"&"
+                        +URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String result="";
+                String line="";
+                while((line = bufferedReader.readLine())!= null) {
+                    result += line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
         return null;
     }
--- app/src/main/java/com/Mystiko/smartmeterautomation/MainActivity.java
+++ app/src/main/java/com/Mystiko/smartmeterautomation/MainActivity.java
@@ -1,5 +1,6 @@
 package com.Mystiko.smartmeterautomation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -27,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
         String type = "login";
         BackgroundWorker backgroundWorker = new BackgroundWorker(this);
         backgroundWorker.execute(type, username, password);
+    }
+
+    public void OpenReg(View view){
+        startActivity(new Intent(MainActivity.this,Register.class));
     }
 
 }
