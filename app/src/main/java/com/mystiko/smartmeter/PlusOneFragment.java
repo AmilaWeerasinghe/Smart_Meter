5750c6b229f56bb4ad54f4d427fc24b60253e4b7
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2019-06-04 18:19:10 +0530

    Usage and home page upgraded successfully

--- app/src/main/java/com/mystiko/smartmeter/PlusOneFragment.java
+++ app/src/main/java/com/mystiko/smartmeter/PlusOneFragment.java
@@ -1,6 +1,7 @@
 package com.mystiko.smartmeter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -123,5 +124,11 @@ public class PlusOneFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+    public void returntoHome(View view) {
+        Intent intent = new Intent(getActivity(), UserHome.class);
+        startActivity(intent);
+    }
+
 
 }
